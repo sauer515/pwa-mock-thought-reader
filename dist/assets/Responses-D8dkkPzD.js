709a0174e78c2cf0b6b60338ab3c237371623314
sauer515 <147532362+sauer515@users.noreply.github.com>
2025-06-23 13:56:20 +0200

    update dependencies and add new script to publish on gh-pages

---
+++ dist/assets/Responses-D8dkkPzD.js
@@ -0,0 +1,1 @@
+const e={"chi-01":["My favorite color is blue."],"chi-02":["I prefer tea over coffee."],"chi-03":["I don't like chocolate."],"chi-04":["Yes I love hiking in the mountains."]};export{e as RESPONSES};
